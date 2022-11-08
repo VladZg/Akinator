c66a9575e68b652d8fe5dd83bc0328c54231ee89
VladZg <vlad.v.zag@gmail.com>
2022-11-09 01:41:36 +0300

    middle commit

--- Config.h
+++ Config.h
@@ -1,1 +1,2 @@
 #define NLOGGING
+// #define NOPEN_DUMPS
--- main.cpp
+++ main.cpp
@@ -24,25 +24,29 @@ int NodeConnect(Node* left, Node* right, Node* root);
 void NodePreorderPrint (const Node* node, FILE* stream);
 void NodeInorderPrint  (const Node* node, FILE* stream);
 void NodePostorderPrint(const Node* node, FILE* stream);
+int IsObjectExist(const Node* node, const char* obj_name);
 
 #define DATABASE_FILENAME "./Database.txt"
 #define TEXT_FOR_PRONOUNCING_FILENAME "./TextForPronouncing.txt"
+#define DATABASE_DUMP_HTML_FILE "./DatabaseDump.html"
 
 int ReadDatabaseToTree(FILE* database_file, Tree* tree);
 int WriteTreeInDatabase(FILE* database_file, Tree* tree);
 void WriteNodeInDatabase(FILE* database_file, Node* node);
+int WriteObjectDefinition(FILE* out_stream, FILE* pronouncing_stream, Node* node, const char* object_name);
 int StartGame();
 int FinishGame();
-int StartGuessMode();
-int GuessObject(const char* object_name);
-int StartDefinitionMode();
-int GiveObjectDefinition(const char* object_name);
-int ComapareObjects();
+int StartGuessMode(Tree* tree);
+int GuessObject(Node* node, const char* object_name);
+int StartDefinitionMode(Tree* tree);
+int GiveObjectDefinition(Tree* tree, const char* object_name);
+int StartComparingMode(Tree* tree);
+int ComapareObjects(Tree* tree, const char* object1_name, const char* object2_name);
 int ShowDatabase();
 int ShowModes();
 int ShowRools();
 int CleanInputBuffer();
-void PrintPath(int mode);
+void PrintPath(int mode, FILE* stream);
 
 char Root_data[] = "Неизвестно кто";
 size_t Database_format_shift = 0;
@@ -129,44 +133,44 @@ int CleanInputBuffer()
     return 1;
 }
 
-void PrintPath(int mode)
+void PrintPath(int mode, FILE* stream)
 {
     if (mode == MENU_MODE)
     {
-        fprintf(stdout, KBLU "~/" KNRM "$ ");
+        fprintf(stream, KBLU "~/" KNRM "$ ");
         return;
     }
 
-    fprintf(stdout, KBLU "~/menu");
+    fprintf(stream, KBLU "~/menu");
 
     switch (mode)
     {
         case GUESSING_MODE:
         {
-            fprintf(stdout, "/GuessingMode");
+            fprintf(stream, "/GuessingMode");
             break;
         }
 
         case DEFINITION_MODE:
         {
-            fprintf(stdout, "/DefinitionMode");
+            fprintf(stream, "/DefinitionMode");
             break;
         }
 
         case COMPARING_MODE:
         {
-            fprintf(stdout, "/ComparingMode");
+            fprintf(stream, "/ComparingMode");
             break;
         }
 
         case SHOW_MODE:
         {
-            fprintf(stdout, "/ShowMode");
+            fprintf(stream, "/ShowMode");
             break;
         }
     }
 
-    fprintf(stdout, KNRM "$ ");
+    fprintf(stream, KNRM "$ ");
 }
 
 int StartGame()
@@ -202,6 +206,16 @@ int StartGame()
     NodeCtor(&node2, str2);
     NodeConnect(&node1, &node2, &root);
 
+    char str3[] = "aboba1";
+    Node node3 = {};
+    NodeCtor(&node3, str3);
+    NodeConnect(&node3, nullptr, &node1);
+
+    char str4[] = "aboba2";
+    Node node4 = {};
+    NodeCtor(&node4, str4);
+    NodeConnect(nullptr, &node4, &node2);
+
 //     fprintf(stdout, "Preorder: ");
 //     NodePreorderPrint(&root, stdout);
 //     fprintf(stdout, "\n");
@@ -217,25 +231,25 @@ int StartGame()
     do
     {
         fprintf(stdout, "\n");
-        PrintPath(NO_MODE);
+        PrintPath(NO_MODE, stdout);
 
         switch(mode = getchar())
         {
             case '1':
             {
-                StartGuessMode();
+                StartGuessMode(&tree);
                 break;
             }
 
             case '2':
             {
-                StartDefinitionMode();
+                StartDefinitionMode(&tree);
                 break;
             }
 
             case '3':
             {
-                ComapareObjects();
+                StartComparingMode(&tree);
                 break;
             }
 
@@ -265,7 +279,7 @@ int StartGame()
 
             default:
             {
-                PrintPath(NO_MODE);
+                PrintPath(NO_MODE, stdout);
                 fprintf(stdout, "Такого режима не существует выберете заново:");
                 break;
             }
@@ -286,15 +300,15 @@ int StartGame()
 
 int ShowModes()
 {
-    PrintPath(MENU_MODE);
+    PrintPath(MENU_MODE, stdout);
     fprintf(stdout, "Modes:\n");
-    PrintPath(MENU_MODE);
+    PrintPath(MENU_MODE, stdout);
     fprintf(stdout, "(1)Угадать объект\n");
-    PrintPath(MENU_MODE);
+    PrintPath(MENU_MODE, stdout);
     fprintf(stdout, "(2)Дать определение существующему объекту\n");
-    PrintPath(MENU_MODE);
+    PrintPath(MENU_MODE, stdout);
     fprintf(stdout, "(3)Сравнить 2 существующих объекта\n");
-    PrintPath(MENU_MODE);
+    PrintPath(MENU_MODE, stdout);
     fprintf(stdout, "(4)Отобразить существующую базу данных\n");
 
     return 1;
@@ -303,11 +317,11 @@ int ShowModes()
 int ShowRools()
 {
 
-    PrintPath(MENU_MODE);
+    PrintPath(MENU_MODE, stdout);
     fprintf(stdout, "Выберите режим из предложенных в меню (команда 'm')\n");
-    PrintPath(MENU_MODE);
+    PrintPath(MENU_MODE, stdout);
     fprintf(stdout, "Для возврата из режима в главное меню нажмите 'q', находясь а выбранном режиме\n");
-    PrintPath(MENU_MODE);
+    PrintPath(MENU_MODE, stdout);
     fprintf(stdout, "Для выхода из игры нажмите 'q', находясь в главном меню\n");
 
     return 1;
@@ -336,11 +350,21 @@ int NodeCtor(Node* node, char* data)
 
 int NodeConnect(Node* left, Node* right, Node* root)
 {
-    root->left  = left;
-    root->right = right;
+    if (root)
+    {
+        root->left  = left;
+        root->right = right;
+    }
 
     return 1;
 }
+
+// Node* FindNode(const Tree* tree, const char* data)
+// {
+//
+//
+//     return &node;
+// }
 
 void NodePreorderPrint(const Node* node, FILE* stream)
 {
@@ -387,32 +411,43 @@ void NodePostorderPrint(const Node* node, FILE* stream)
     fprintf(stream, ")");
 }
 
-int IsObectExist(const char* name)
+int IsObjectExist(const Node* node, const char* obj_name)
 {
+    if (!strcasecmp(node->data, obj_name))
+        return 1;
 
-    return 1;
+    if (!(node->left || node->right))
+        return 0;
+
+    if (node->left)
+        if (IsObjectExist(node->left, obj_name)) return 1;
+
+    if (node->right)
+        if (IsObjectExist(node->right, obj_name)) return 1;
+
+    return 0;
 }
 
-int StartGuessMode()
+int StartGuessMode(Tree* tree)
 {
     CleanInputBuffer();
 
-    PrintPath(GUESSING_MODE);
+    PrintPath(GUESSING_MODE, stdout);
     fprintf(stdout, "Введите название объекта:\n");
-    PrintPath(GUESSING_MODE);
+    PrintPath(GUESSING_MODE, stdout);
 
     char object_name[MAX_OBJECT_NAME] = {};
     fscanf(stdin, "%[^\n]s", object_name);
 
-    while (strcmp(object_name, "q"))
+    while (strcasecmp(object_name, "q"))
     {
-        GuessObject(object_name);
+        GuessObject(tree->root, object_name);
 
         CleanInputBuffer();
 
-        PrintPath(GUESSING_MODE);
+        PrintPath(GUESSING_MODE, stdout);
         fprintf(stdout, "Введите назвние нового объекта:\n");
-        PrintPath(GUESSING_MODE);
+        PrintPath(GUESSING_MODE, stdout);
 
         fscanf(stdin, "%[^\n]s", object_name);
     }
@@ -420,34 +455,42 @@ int StartGuessMode()
     return 1;
 }
 
-int GuessObject(const char* object_name)
+int GuessObject(Node* node, const char* object_name)
 {
-    PrintPath(GUESSING_MODE);
-    fprintf(stdout, "Objected \"%s\" guessed!\n\n", object_name);
+    PrintPath(GUESSING_MODE, stdout);
+
+    if (IsObjectExist(node, object_name))
+    {
+        fprintf(stdout, "Objected \"%s\" guessed!\n\n", object_name);
+
+    }
+
+    else
+        fprintf(stdout, "There is no such object\n\n");
 
     return 1;
 }
 
-int StartDefinitionMode()
+int StartDefinitionMode(Tree* tree)
 {
     CleanInputBuffer();
 
-    PrintPath(DEFINITION_MODE);
+    PrintPath(DEFINITION_MODE, stdout);
     fprintf(stdout, "Введите название объекта, которому хотите дать определение:\n");
-    PrintPath(DEFINITION_MODE);
+    PrintPath(DEFINITION_MODE, stdout);
 
     char object_name[MAX_OBJECT_NAME] = {};
     fscanf(stdin, "%[^\n]s", object_name);
 
-    while (strcmp(object_name, "q"))
+    while (strcasecmp(object_name, "q"))
     {
-        GiveObjectDefinition(object_name);
+        GiveObjectDefinition(tree, object_name);
 
         CleanInputBuffer();
 
-        PrintPath(DEFINITION_MODE);
+        PrintPath(DEFINITION_MODE, stdout);
         fprintf(stdout, "Введите назвние объекта, которому хотите дать определение:\n");
-        PrintPath(DEFINITION_MODE);
+        PrintPath(DEFINITION_MODE, stdout);
 
         fscanf(stdin, "%[^\n]s", object_name);
     }
@@ -455,15 +498,35 @@ int StartDefinitionMode()
     return 1;
 }
 
-int GiveObjectDefinition(const char* object_name)
+int WriteObjectDefinition(FILE* out_stream, FILE* pronouncing_stream, Node* node, const char* object_name)
+{
+    PrintPath(DEFINITION_MODE, out_stream);
+    fprintf(out_stream, "test");
+    fprintf(pronouncing_stream, "test");
+
+    fprintf(out_stream, "\n\n");
+
+    return 1;
+}
+
+int GiveObjectDefinition(Tree* tree, const char* object_name)
 {
     FILE* file = fopen(TEXT_FOR_PRONOUNCING_FILENAME, "w+");
 
-    PrintPath(DEFINITION_MODE);
-    fprintf(stdout, "Object \"%s\" definition:\n\n", object_name);
+    PrintPath(DEFINITION_MODE, stdout);
 
-    fprintf(file, "Object \"%s\" definition:\n\n", object_name);
-    fprintf(file, "Some definition here\n");
+    if (IsObjectExist(tree->root, object_name))
+    {
+        fprintf(stdout, "Object \"%s\" definition:\n", object_name);
+        fprintf(file, "Object \"%s\" definition:\n", object_name);
+        WriteObjectDefinition(stdout, file, tree->root, object_name);
+    }
+
+    else
+    {
+        fprintf(stdout, "There is no object called \"%s\"\n\n", object_name);
+        fprintf(file, "There is no object called \"%s\"\n\n", object_name);
+    }
 
     fclose(file);
 
@@ -472,18 +535,104 @@ int GiveObjectDefinition(const char* object_name)
     return 1;
 }
 
-int ComapareObjects()
+int StartComparingMode(Tree* tree)
 {
-    PrintPath(COMPARING_MODE);
-    fprintf(stdout, "Comparing mode:\n");
+    CleanInputBuffer();
+
+    PrintPath(COMPARING_MODE, stdout);
+    fprintf(stdout, "Введите название первого объекта сравнения:\n");
+    PrintPath(COMPARING_MODE, stdout);
+    char object1_name[MAX_OBJECT_NAME] = {};
+    fscanf(stdin, "%[^\n]s", object1_name);
+
+    CleanInputBuffer();
+
+    PrintPath(COMPARING_MODE, stdout);
+    fprintf(stdout, "Введите название второго объекта сравнения:\n");
+    PrintPath(COMPARING_MODE, stdout);
+    char object2_name[MAX_OBJECT_NAME] = {};
+    fscanf(stdin, "%[^\n]s", object2_name);
+
+    while (strcasecmp(object1_name, "q") && strcasecmp(object2_name, "q"))
+    {
+        ComapareObjects(tree, object1_name, object2_name);
+
+        CleanInputBuffer();
+
+        PrintPath(COMPARING_MODE, stdout);
+        fprintf(stdout, "Введите название первого объекта сравнения:\n");
+        PrintPath(COMPARING_MODE, stdout);
+        fscanf(stdin, "%[^\n]s", object1_name);
+
+        CleanInputBuffer();
+
+        PrintPath(COMPARING_MODE, stdout);
+        fprintf(stdout, "Введите название второго объекта сравнения:\n");
+        PrintPath(COMPARING_MODE, stdout);
+        fscanf(stdin, "%[^\n]s", object2_name);
+    }
 
     return 1;
 }
 
+int ComapareObjects(Tree* tree, const char* object1_name, const char* object2_name)
+{
+    PrintPath(COMPARING_MODE, stdout);
+    // fprintf(stdout, "Comparing mode:\n");
+
+    if (IsObjectExist(tree->root, object1_name) && IsObjectExist(tree->root, object2_name))
+    {
+        fprintf(stdout, "Both objects exist\n\n");
+        return 1;
+    }
+
+    else if (IsObjectExist(tree->root, object1_name))
+    {
+        fprintf(stdout, "Object \"%s\" doesn't exist\n\n", object2_name);
+        return 0;
+    }
+
+    else if (IsObjectExist(tree->root, object2_name))
+    {
+        fprintf(stdout, "Object \"%s\" doesn't exist\n\n", object1_name);
+        return 0;
+    }
+
+    fprintf(stdout, "No one of this objects exist\n\n");
+
+    return 0;
+}
+
 int ShowDatabase()
 {
-    PrintPath(SHOW_MODE);
-    fprintf(stdout, "Show database mode:\n");
+    // PrintPath(SHOW_MODE, stdout);
+    // fprintf(stdout, "Show database mode:\n");
+
+    system("dot ./TextForDatabaseDump.dot -Tsvg -o ./DatabaseDump.svg");
+
+    FILE* file_html = fopen(DATABASE_DUMP_HTML_FILE, "w+");
+
+    fseek(file_html, 0, SEEK_SET);
+
+    fprintf(file_html, "<pre>\n");
+    fprintf(file_html, "    <h1> DATABASE (%s) </h1>\n", "time");
+
+    #include "./UndefColourConsts.h"
+    // ListTextDump(list, file_html);
+    #include "./DefineColourConsts.h"
+
+    fprintf(file_html,  "    \n"
+                        "        <img src = \"./DatabaseDump.svg\">\n"
+                        "    <hr>\n"
+                        "<!-- ------------------------------------------------------------ -->\n");
+
+    fprintf(file_html, "</pre>\n\n");
+
+    fclose(file_html);
+
+    #ifndef NOPEN_DUMPS
+    system("xdg-open \"./DatabaseDump.html\"");
+    #endif
 
     return 1;
 }
